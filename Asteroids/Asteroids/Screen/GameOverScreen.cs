f711947afb87071070904804104e4f4fec2d84a6
Sean Legg <leggs@tcd.ie>
2012-10-09 15:00:26 +0100

    Added GameOver Screen

--- Asteroids/Asteroids/Screen/GameOverScreen.cs
+++ Asteroids/Asteroids/Screen/GameOverScreen.cs
@@ -7,10 +7,15 @@ using Microsoft.Xna.Framework.Input;
 namespace Asteroids
 {
     class GameOverScreen : GameScreen
-    {        
+    {
+        private SpriteFont textFont;
+        private Text gameOverMessage;
+
         public GameOverScreen(ContentManager content)
         {
-            
+            textFont = content.Load<SpriteFont>("font/Menu");
+
+            gameOverMessage = new Text("Game Over", textFont);
         }
 
         public void Init()
@@ -25,12 +30,21 @@ namespace Asteroids
 
         public override void Update(GameTime dt)
         {
+            InputManager iManager = InputManager.Instance;
 
+            if (iManager.IsKeyPressed(Keys.Enter) || iManager.IsButtonPressed(Buttons.Start))
+            {
+                EventManager.Instance.Publish(new Event(EventType.NAVIGATE_MAIN_MENU));
+            }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            
+            spriteBatch.Begin();
+            {
+                spriteBatch.DrawString(textFont, gameOverMessage.title, gameOverMessage.center, Color.Green);
+            }
+            spriteBatch.End();
         }
     }
 }
